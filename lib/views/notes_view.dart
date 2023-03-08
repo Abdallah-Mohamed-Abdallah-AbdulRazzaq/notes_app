a98d90acf6b607bf5f68feb87296de0f2f34b038
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-03-08 19:09:12 +0200

    Change theme and creat notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+//Change theme and creat notes view 
+
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+ 
 
     return Scaffold();
   }
