3935bb728a5641c62417ae268005f4a8dcf48e79
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-03-08 20:39:38 +0200

    Add widget (NotesViewBody) inside the body

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    
+  }
+}
