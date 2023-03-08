fc1936793e1b7394bc1b584a9e7c4004f8d54ee5
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-03-08 20:45:21 +0200

    A scaffold with a (import NotesViewBody) inside it

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,14 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 //Change theme and creat notes view 
+
+import 'widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
- 
-
-    return Scaffold();
+    return  const Scaffold(
+    body: NotesViewBody(),
+    );
   }
 }
