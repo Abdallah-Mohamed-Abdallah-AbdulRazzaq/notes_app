9e6aef9e6a0b44c7398074e8facb6e06e70c675e
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-03-08 23:18:52 +0200

    Add  height =46 in CustomSearchIcon

---
+++ lib/views/widgets/custom_icon.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+    );
+  }
+}
