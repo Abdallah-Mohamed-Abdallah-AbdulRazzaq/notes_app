57438c8eb2b7b101886528ece909f5ebef9a0499
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 17:04:57 +0200

    Create new file (add _note _bottom_sheet.dart)

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -24,14 +24,7 @@ class NotesView extends StatelessWidget {
   }
 }
 
-class AddNoteBottomSheet extends StatelessWidget {
-  const AddNoteBottomSheet ({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
 
 
 
