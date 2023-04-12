9e3c8ac214a98bc8ec43c9663c2191d91b1e404c
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 19:37:23 +0200

    Add SizedBox between Title and Content

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -16,6 +16,7 @@ class AddNoteBottomSheet extends StatelessWidget {
           CustomTextField(
             hint: 'Title' ,
           ),
+          SizedBox(height: 16),
           CustomTextField(
             hint: 'Content' ,
             maxLines: 5,
