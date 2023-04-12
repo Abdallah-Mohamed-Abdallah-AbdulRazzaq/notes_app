fca23ea47271da932f56b8eebf62d927a1075fc6
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 15:59:31 +0200

    Create file (custom_note_item.dart)

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/views/widgets/custom_app_bar.dart';
 
+import 'custom_note_item.dart';
+
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
@@ -25,41 +27,3 @@ class NotesViewBody extends StatelessWidget {
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24 , bottom: 24 , left: 16),
-
-      decoration: BoxDecoration(
-        color:const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-
-            title: const Text('Flutter Tips' , style: TextStyle(color: Colors.black ,fontSize: 26),),
-
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text('build your career with Abdallah Mohamed', style: TextStyle(color: Colors.black.withOpacity(.4) , fontSize: 20),),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete ,color: Colors.black,),
-               )
-          ),
-         
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('May , 2022' , style: TextStyle(color: Colors.black.withOpacity(.4),fontSize: 16),),
-          )
-        ],
-      ),
-    );
-  }
-}
