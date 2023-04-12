a777948682b24dc421e8c3b10548de8ac8b35ffc
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 20:25:48 +0200

    import constants.dart

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/constants.dart';
 
+import 'custom_button.dart';
 import 'custom_text_field.dart';
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet ({Key? key}) : super(key: key);
@@ -29,23 +30,6 @@ class AddNoteBottomSheet extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(color: kPrimaryColor , borderRadius: BorderRadius.circular(8)),
-      child: const Center(
-        child: Text('Add',style: TextStyle(color: Colors.black , fontWeight: FontWeight.bold , fontSize: 20),),
-      ),
-
     );
   }
 }
