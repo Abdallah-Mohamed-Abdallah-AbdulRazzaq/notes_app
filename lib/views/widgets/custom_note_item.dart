77c2e459bc4ac09f8f348b4f9d316bfc73e02e33
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 20:49:53 +0200

    return GestureDetector

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -4,35 +4,38 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24 , bottom: 24 , left: 16),
-
-      decoration: BoxDecoration(
-        color:const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-
-            title: const Text('Flutter Tips' , style: TextStyle(color: Colors.black ,fontSize: 26),),
-
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text('build your career with Abdallah Mohamed', style: TextStyle(color: Colors.black.withOpacity(.4) , fontSize: 20),),
+    return GestureDetector(
+      
+      child: Container(
+        padding: const EdgeInsets.only(top: 24 , bottom: 24 , left: 16),
+    
+        decoration: BoxDecoration(
+          color:const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+    
+              title: const Text('Flutter Tips' , style: TextStyle(color: Colors.black ,fontSize: 26),),
+    
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('build your career with Abdallah Mohamed', style: TextStyle(color: Colors.black.withOpacity(.4) , fontSize: 20),),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete ,color: Colors.black,),
+                 )
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete ,color: Colors.black,),
-               )
-          ),
-         
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text('May , 2022' , style: TextStyle(color: Colors.black.withOpacity(.4),fontSize: 16),),
-          )
-        ],
+           
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text('May , 2022' , style: TextStyle(color: Colors.black.withOpacity(.4),fontSize: 16),),
+            )
+          ],
+        ),
       ),
     );
   }
