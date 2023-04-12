c67ec2e0a8df9ac88ccf58746a7c110cf3f701df
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 23:37:31 +0200

    add extends(HiveObject) and import hive.dart

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,10 +1,16 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+class NoteModel extends HiveObject {
   final String title;
   final String subTitle;
   final String data;
   final int color;
 
-  NoteModel({ required this.title , required this.subTitle , required this.data , required this.color});
+  NoteModel({ required this.title,
+    required this.subTitle,
+    required this.data,
+    required this.color});
+
 
 
 
