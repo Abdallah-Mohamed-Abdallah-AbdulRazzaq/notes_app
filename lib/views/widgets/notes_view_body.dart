2a6f46600abf8a5adfec176db8c0c2b6bd75ccc6
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 15:43:05 +0200

    Add Widget padding in the (Container)

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -31,6 +31,7 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.only(top: 24 , bottom: 24 , left: 16),
 
       decoration: BoxDecoration(
         color:const Color(0xffFFCC80),
