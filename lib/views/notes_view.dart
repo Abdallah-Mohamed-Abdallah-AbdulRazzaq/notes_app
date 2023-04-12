1bb07df040d0a85bdd4193f236fb24f21360d2a0
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 16:59:19 +0200

    Add Widget (class AddNoteBottomSheet)

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -23,3 +23,14 @@ class NotesView extends StatelessWidget {
     );
   }
 }
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
