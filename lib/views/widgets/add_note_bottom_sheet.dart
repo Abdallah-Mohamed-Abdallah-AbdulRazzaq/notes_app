62cfd5d9187e8a2f68d1d5c16e38370c7bc8311f
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 20:18:13 +0200

    SizedBox(height = 50)

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -23,7 +23,7 @@ class AddNoteBottomSheet extends StatelessWidget {
               hint: 'Content' ,
               maxLines: 5,
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 50),
             CustomButton(),
             SizedBox(height: 16),
           ],
