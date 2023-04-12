6803e35db8654e311ed0dfdf0910a8c728b71044
Abdallah-Mohamed-Abdallah-AbdulRazzaq <abdallahmohammad66446@gmail.com>
2023-04-12 18:04:50 +0200

    Add cursorColor (kPrimaryColor)

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../../constants.dart';
+
 class CustomTextField extends StatelessWidget {
   const CustomTextField({super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      cursorColor: kPrimaryColor,
       decoration: InputDecoration(border: buildBorder(),
       enabledBorder: buildBorder(),
-      focusedBorder: buildBorder(const Color(0xff62FCD7)),
+      focusedBorder: buildBorder(kPrimaryColor),
       ),
     );
   }
